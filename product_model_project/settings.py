32ec28d9b228eeb066ffa46cb236a5df65a61870
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-12-09 05:52:11 -0500

    changed database settings + created mysql database + ran migrations

--- product_model_project/settings.py
+++ product_model_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-30t*0+ssw=v+@6q+%^@rj9jjsu@5t^$2c@^892-p9&d0^zud%-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'product_model_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -109,7 +98,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -121,3 +110,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from product_model_project.local_settings import *
+except ImportError:
+    pass
