c420fdf2134658327359d4d11c6870157dfe7bb0
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-12-09 12:47:06 -0500

    created postman collection + created GET HTTP command on postman + added serializers to product model views file

--- product_model/views.py
+++ product_model/views.py
@@ -1,6 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductModelSerializer
+from .models import ProductModel
 
 @api_view(['GET'])
 def product_model_list(request):
-    return Response('ok')
+    cars = ProductModel.objects.all()
+    serializer = ProductModelSerializer(cars, many=True)
+    return Response(serializer.data)
